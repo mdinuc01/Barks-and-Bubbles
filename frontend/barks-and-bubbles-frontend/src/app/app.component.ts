4eb1fbf7b18a84b2823fa59e241c88288ac43b6f
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-04-29 16:14:32 -0400

    feat: added DataService and HttpClient providers to app.component

--- frontend/barks-and-bubbles-frontend/src/app/app.component.ts
+++ frontend/barks-and-bubbles-frontend/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './services/data.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [DataService, HttpClient],
 })
 export class AppComponent {
   title = 'barks-and-bubbles-frontend';
