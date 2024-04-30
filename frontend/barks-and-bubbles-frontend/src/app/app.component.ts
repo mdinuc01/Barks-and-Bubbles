65fd58360a0ab7fba2106ce3398a70ec15a10675
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-04-29 23:33:15 -0400

    fix: added DataService and new components to app providers

--- frontend/barks-and-bubbles-frontend/src/app/app.component.ts
+++ frontend/barks-and-bubbles-frontend/src/app/app.component.ts
@@ -2,14 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DataService } from './services/data.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { ClientListComponent } from './components/client-list/client-list.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule],
+  imports: [RouterOutlet, HttpClientModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  providers: [DataService, HttpClient],
+  providers: [DataService, HttpClient, ClientListComponent, HomeComponent],
 })
 export class AppComponent {
   title = 'barks-and-bubbles-frontend';
