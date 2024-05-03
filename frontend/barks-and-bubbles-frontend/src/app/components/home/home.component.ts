7084c4138db05b339f5a5fed28393ce23c99b00e
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-05-02 23:20:16 -0400

    fix: added imports for the AppointmentList component

--- frontend/barks-and-bubbles-frontend/src/app/components/home/home.component.ts
+++ frontend/barks-and-bubbles-frontend/src/app/components/home/home.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { FileReaderComponent } from '../file-reader-component/file-reader-component.component';
 import { ClientListComponent } from '../client-list/client-list.component';
 import { MatButtonModule } from '@angular/material/button';
+import { AppointmentListComponent } from '../appointment-list/appointment-list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [FileReaderComponent, ClientListComponent, MatButtonModule],
+  imports: [
+    FileReaderComponent,
+    ClientListComponent,
+    MatButtonModule,
+    AppointmentListComponent,
+  ],
 })
 export class HomeComponent {}
