7cffb6e05ca7ca35c60b36a554b3ff358bee6f8a
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-05-02 23:18:21 -0400

    fix: fixed formatting issues in the file-reader-service

--- frontend/barks-and-bubbles-frontend/src/app/components/file-reader-component/file-reader-component.component.ts
+++ frontend/barks-and-bubbles-frontend/src/app/components/file-reader-component/file-reader-component.component.ts
@@ -1,18 +1,18 @@
-import { DataService } from './../../services/data.service';
-import { Component } from '@angular/core';
+import { DataService } from '../../services/data.service';
 import { ExcelParserService } from '../../services/excelParser';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-file-reader-component',
   standalone: true,
   imports: [],
   templateUrl: './file-reader-component.component.html',
-  styleUrl: './file-reader-component.component.scss',
+  styleUrls: ['./file-reader-component.component.scss'],
 })
 export class FileReaderComponent {
   constructor(
     private excelParserService: ExcelParserService,
-    private DataService: DataService
+    private dataService: DataService
   ) {}
 
   async onFileChange(event: any) {
@@ -22,7 +22,7 @@ export class FileReaderComponent {
         const parsedData: any[] = await this.excelParserService.parseExcelFile(
           file
         );
-        this.DataService.addClientsToTable(parsedData);
+        this.dataService.addClientsToTable(parsedData);
       } catch (error) {
         console.error('Error parsing Excel file:', error);
       }
