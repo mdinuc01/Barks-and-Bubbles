7d8ef2041d795feb9742aff97d19980e477ee875
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-05-07 21:58:32 -0400

    feat: created SMSUtils to handle sending texts and loading replies

---
+++ backend/utils/SMSUtils.js
@@ -0,0 +1,87 @@
+require('dotenv').config();
+const axios = require('axios');
+const Message = require('../models/Message.js');
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const phone = process.env.TWILIO_PHONE_NUMBER;
+
+const twilio = require('twilio');
+const client = new twilio.Twilio(accountSid, authToken);
+
+class SMSUtils {
+
+  async getReplies(date) {
+
+    const url = `https://api.twilio.com/2010-04-01/Accounts/${accountSid}/Messages.json`;
+
+    const formattedDate = new Date(date);
+    const dateAfter = formattedDate.toISOString();
+    const dateBefore = new Date().toISOString();
+
+    const auth = {
+      username: accountSid,
+      password: authToken
+    };
+
+    try {
+      // const response = await axios.get(url, {
+      //   params: {
+      //     dateSentAfter: dateAfter,
+      //     dateSentBefore: dateBefore,
+
+      //   },
+      //   auth: auth
+      // });
+
+      // return response.data.messages;
+      let res;
+
+      await client.messages
+        .list({
+          dateSentAfter: dateAfter,
+          dateSentBefore: dateBefore,
+        })
+        .then(messages => res = messages);
+
+      return res;
+    } catch (error) {
+      console.error('Error fetching messages:', error);
+      throw error;
+    }
+  }
+
+  async sendText(clientDate, date) {
+    let message = new Message(clientDate, date);
+    let number = `+1${clientDate.contactMethod}`;
+
+    try {
+      const messageObj = await client.messages
+        .create({
+          body: message.createMessage(),
+          from: phone,
+          to: number
+        })
+      return messageObj;
+    } catch (error) {
+      console.log({ error })
+    }
+  }
+
+  async sendReply(clientDate, date) {
+    let message = new Message(clientDate, date);
+    let number = `+1${clientDate.contactMethod}`;
+
+    try {
+      const messageObj = await client.messages
+        .create({
+          body: message.createMessage(),
+          from: phone,
+          to: number
+        })
+      return messageObj;
+    } catch (error) {
+      console.log({ error })
+    }
+  }
+}
+module.exports = new SMSUtils();
