68d701e6ce6573d98d41dc759e5d9d29cd2e8a36
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-05-07 22:01:32 -0400

    feat: added reply message template to Message model

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -6,7 +6,11 @@ class Message {
   }
 
   createMessage() {
-    return `Hi ${this.name}! We’re back for nail trimming ${this.date}! Please give us a \u{1F44D} if you’d like us to swing by for ${this.petName}’s nail trim, and we’ll get back to you a day prior with a timeframe \u{1F43E}`
+    return `Hi ${this.name}! We’re back for nail trimming on ${this.date}! Please give us a \u{1F44D} if you’d like us to swing by for ${this.petName}’s nail trim, and we’ll get back to you a day prior with a timeframe \u{1F43E}`
+  }
+
+  createReply() {
+    return `Hi ${this.name}! Aiming to be there around ${this.date} tomorrow \u{1F60A}`
   }
 }
 
