5e7a25cbf983c6ad61973e6161e1fe8ce6154fb8
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-05-10 22:28:43 -0400

    feat: added functionality to view messages

--- frontend/barks-and-bubbles-frontend/src/app/components/appointment-page/appointment-page.component.ts
+++ frontend/barks-and-bubbles-frontend/src/app/components/appointment-page/appointment-page.component.ts
@@ -1,3 +1,4 @@
+import { ToastService } from './../../services/toast.service';
 import { DataService } from './../../services/data.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -9,28 +10,59 @@ import { MatCardModule } from '@angular/material/card';
 import { PanelService } from '../../services/panel service/panel-service';
 import { MatDialog } from '@angular/material/dialog';
 import { lastValueFrom } from 'rxjs';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { AppointmentSchedulerComponent } from '../appointment-scheduler/appointment-scheduler.component';
 
 @Component({
   selector: 'app-appointment-page',
   standalone: true,
+  templateUrl: './appointment-page.component.html',
+  styleUrl: './appointment-page.component.scss',
+  animations: [
+    trigger('panelState', [
+      state(
+        'open',
+        style({
+          transform: 'translateX(0%)',
+        })
+      ),
+      state(
+        'closed',
+        style({
+          transform: 'translateX(100%)',
+        })
+      ),
+      transition('open => closed', [animate('0.2s')]),
+      transition('closed => open', [animate('0.2s')]),
+    ]),
+  ],
   imports: [
     CommonModule,
     MatButtonModule,
     MatIconModule,
     MatTooltipModule,
     MatCardModule,
+    AppointmentSchedulerComponent,
   ],
-  templateUrl: './appointment-page.component.html',
-  styleUrl: './appointment-page.component.scss',
 })
 export class AppointmentPageComponent implements OnInit {
   appointment: any;
   loadingReplies = false;
+  loadCount = 0;
+  panelState: string = 'closed';
+  isPanelOpen: boolean = false;
 
   constructor(
     public DataService: DataService,
     private activatedRoute: ActivatedRoute,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private ToastService: ToastService
   ) {}
 
   ngOnInit(): void {
@@ -84,7 +116,8 @@ export class AppointmentPageComponent implements OnInit {
         true,
         false
       );
-      this.DataService.sendText(
+
+      await this.DataService.sendText(
         this.appointment.meta,
         date,
         this.appointment.id
@@ -105,5 +138,41 @@ export class AppointmentPageComponent implements OnInit {
     if (this.appointment.messages)
       return Object.keys(this.appointment.messages).length > 0;
     else return false;
+  }
+
+  showCard(message: any, client: any) {
+    let to = message.to.replaceAll('+1', '');
+    let from = message.from.replaceAll('+1', '');
+
+    if (message.direction.includes('outbound') && client.contactMethod == to)
+      return true;
+    else if (
+      message.direction.includes('inbound') &&
+      client.contactMethod == from
+    )
+      return true;
+    else return false;
+  }
+
+  messageSuccess() {
+    this.ToastService.showSuccess('Messages Sent! Please load messages!');
+  }
+
+  formatStatus(status: string): string {
+    if (status.length === 0) {
+      return status; // Return the word unchanged if it's empty
+    }
+
+    return status.charAt(0).toUpperCase() + status.slice(1);
+  }
+  togglePanel(event: Event, outside: boolean) {
+    event.stopPropagation();
+    if (this.panelState == 'closed' && outside) return;
+    this.panelState = this.panelState === 'open' ? 'closed' : 'open';
+    this.isPanelOpen = !this.isPanelOpen;
+  }
+
+  receiveReplies(data: any) {
+    console.log({ data });
   }
 }
