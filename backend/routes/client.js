5d46b4e29442ba0713b3fbadbe3e64562b970eae
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-05-12 09:16:21 -0400

    fix: updated the addClients API route to a post request

--- backend/routes/client.js
+++ backend/routes/client.js
@@ -5,6 +5,6 @@ const router = express.Router();
 
 router.get('/', clientController.getAllClients);
 
-router.put('/add', clientController.addClientsToTable);
+router.post('/add', clientController.addClientsToTable);
 
 module.exports = router;
