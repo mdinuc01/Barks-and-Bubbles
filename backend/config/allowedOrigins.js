d227c709dd11eb477112e2d2770a53f49ae15b21
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-05-27 14:03:16 -0400

    fix: added backend service to allowed origins

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,6 @@
 const allowedOrigins = [
   'http://localhost:4200',
   'https://barks-and-bubbles.netlify.app',
-  'https://barks-and-bubbles.onrender.com/api/pet/'
-
 ]
 
 module.exports = allowedOrigins;
