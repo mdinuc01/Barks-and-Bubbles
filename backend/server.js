fa0ab2a5460224268f871357e3e5209e8ae23912
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-05-27 14:39:01 -0400

    fix: added port binding to server file

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const corsOptions = require('./config/corsOption.js');
 require('dotenv').config();
 const mongoose = require('mongoose');
 let app = express();
+const PORT = process.env.PORT || 8800;
 
 app.use(cors(corsOptions));
 app.use(express.json());
@@ -22,8 +23,8 @@ app.use("/api/message", messageRoute);
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to DB');
-    app.listen(8800, () => {
-      console.log(`Server is running on port 8800`);
+    app.listen(PORT, () => {
+      console.log(`Server is running on PORT PORT`);
     });
   })
   .catch(err => {
