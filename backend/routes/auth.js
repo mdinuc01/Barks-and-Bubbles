aff837114e74d96c4b7aaf9c86dc3f049fd292f4
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-06-25 20:08:31 -0400

    feat: added loginLimiter middleware to login process

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const AuthController = require("../controllers/auth.controller.js");
+const loginLimiter = require('../middleware/loginLimiter.js');
 
 const router = express.Router();
 
 router.post("/signup", AuthController.signup);
-router.post("/signin", AuthController.signIn)
+router.post("/signin", loginLimiter, AuthController.signIn)
 
 module.exports = router;
