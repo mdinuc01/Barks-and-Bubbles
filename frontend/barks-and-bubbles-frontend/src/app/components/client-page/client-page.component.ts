3200461c5c41fb7ec511771e8baed64c199fec46
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-10-16 20:31:31 -0400

    feat: created the client page component

---
+++ frontend/barks-and-bubbles-frontend/src/app/components/client-page/client-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './client-page.component.html',
+  styleUrl: './client-page.component.scss'
+})
+export class ClientPageComponent {
+
+}
