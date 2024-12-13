dd39c08f1c0e80dd7ddee160b72b7808264770b7
Michael Dinuccio <mdinucci@my.centennialcollege.ca>
2024-12-13 11:35:53 -0500

    fix: fixed order of client to match the service area order in the route

--- frontend/barks-and-bubbles-frontend/src/app/components/appointment-client-list/appointment-client-list.component.ts
+++ frontend/barks-and-bubbles-frontend/src/app/components/appointment-client-list/appointment-client-list.component.ts
@@ -53,10 +53,22 @@ export class AppointmentClientListComponent implements OnInit {
         this.petsWithLocations = response.data.allClients;
         this.filteredList = response.data.allClients;
       }
-      if (response.data && response.data.sentClients.length && !this.addToList)
+      if (
+        response.data &&
+        response.data.sentClients.length &&
+        !this.addToList
+      ) {
         this.petsWithLocations = response.data.sentClients;
+        let order = this.appointment.app.route.serviceAreas.map(
+          (a: { name: any }) => a.name
+        );
+        this.petsWithLocations = this.sortObjectsByOrder(
+          order,
+          this.petsWithLocations
+        );
+      }
 
-      if (this.appointment.app && !this.appointment.app.messages)
+      if (this.appointment.app && !this.appointment.app.messages) {
         this.petsWithLocations = response.data.allClients.filter((c: any) => {
           let result = false;
           this.appointment.app.route.serviceAreas.forEach(
@@ -67,6 +79,31 @@ export class AppointmentClientListComponent implements OnInit {
 
           if (result) return c;
         });
+        let order = this.appointment.app.route.serviceAreas.map(
+          (a: { name: any }) => a.name
+        );
+        this.petsWithLocations = this.sortObjectsByOrder(
+          order,
+          this.petsWithLocations
+        );
+      }
+    });
+  }
+
+  sortObjectsByOrder(
+    order: string[],
+    serviceAreas: Record<string, any>[]
+  ): Record<string, any>[] {
+    // Create a map of order indices for quick lookup
+    const orderMap = new Map(order.map((name, index) => [name, index]));
+
+    // Sort the serviceAreas based on the order array
+    return serviceAreas.sort((a, b) => {
+      const nameA = Object.keys(a)[0]; // Get the key (name) of the object
+      const nameB = Object.keys(b)[0];
+      const indexA = orderMap.get(nameA) ?? Infinity; // If not in the order array, push it to the end
+      const indexB = orderMap.get(nameB) ?? Infinity;
+      return indexA - indexB;
     });
   }
 
